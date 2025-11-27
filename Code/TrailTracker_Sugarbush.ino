5731f064ce116ccb839c7a91920a45edac0ac2ab
cpcurtin <71350501+cpcurtin@users.noreply.github.com>
2025-11-27 10:20:08 -0500

    Fixed type error with initialization on trail & lift names. Renamed "Tomy's Toy" to "Easy Up"

--- Code/TrailTracker_Sugarbush.ino
+++ Code/TrailTracker_Sugarbush.ino
@@ -26,59 +26,59 @@ void onLED(Adafruit_NeoPixel, int, int, int, int);
 *************************************************************************************************/
 // Peak order: Lincoln Peak, Gadd Peak, Castlerock Peak, North Lynx Peak, Mt Ellen, Inverness Peak
 unsigned int peakNumber[] = {2, 3, 4, 5, 6, 7};
-const char PeakNameOrder[] = {"Lincoln Peak", "Gadd Peak", "Castlerock Peak", "North Lynx Peak", "Mt Ellen", "Inverness Peak"};
+const char* PeakNameOrder[] = {"Lincoln Peak", "Gadd Peak", "Castlerock Peak", "North Lynx Peak", "Mt Ellen", "Inverness Peak"};
 // Lincoln Peak
 const int LincolnTrailCount = 9;
 const char* LincolnTrailNames[LincolnTrailCount] = {"Jester", "Allyn\'s Traverse", "Organgrinder", "Spillsville", "Sigi\'s Ripcord", "Paradise", "Lower Paradise", "Paradise Extension", "Lower Ripcord"};
 const char *LincolnTrailStatus[LincolnTrailCount] = {}; 
 const char *LincolnTrailRating[LincolnTrailCount] = {};
 const int LincolnLiftCount = 1;
-const char LincolnLiftNames[LincolnLiftCount] = {'Heaven\'s Gate Quad'};
+const char* LincolnLiftNames[LincolnLiftCount] = {"Heaven\'s Gate Quad"};
 const char *LincolnLiftStatus[LincolnLiftCount] = {};
 
 // Gadd Peak
 const int GaddTrailCount= 27;
-const char* GaddTrailNames[GaddTrailCount] = {'Domino Chute', 'Downspout', 'Domino', 'Lower Downspout', 'Lower Domino', 'Heaven\'s Gate Traverse', 'Lower Jester', 'Lower Birdland', 'Lower Organgrinder', 'Murphy\'s Glades', 'Birdland', 'Valley House Traverse', 'Stein\'s Run', 'The Mall', 'Snowball', 'Lixi\'s Twist', 'Moonshine', 'Lower Twist', 'Spring Fling', 'Lower Snowball', 'Spring Road', 'Racer\'s Edge', 'Cat\'s Meow', 'Lower Moonshine', 'Coffee Run', 'Gondolier', 'Header'};
+const char* GaddTrailNames[GaddTrailCount] = {"Domino Chute", "Downspout", "Domino", "Lower Downspout", "Lower Domino", "Heaven\'s Gate Traverse", "Lower Jester", "Lower Birdland", "Lower Organgrinder", "Murphy\'s Glades", "Birdland", "Valley House Traverse", "Stein\'s Run", "The Mall", "Snowball", "Lixi\'s Twist", "Moonshine", "Lower Twist", "Spring Fling", "Lower Snowball", "Spring Road", "Racer\'s Edge", "Cat\'s Meow", "Lower Moonshine", "Coffee Run", "Gondolier", "Header"};
 const char *GaddTrailStatus[GaddTrailCount] = {};
 const char *GaddTrailRating[GaddTrailCount] = {};
 const int GaddLiftCount = 2;
-const char GaddLiftNames[GaddLiftCount] = {'Super Bravo Express Quad', 'Valley House Quad'};
+const char* GaddLiftNames[GaddLiftCount] = {"Super Bravo Express Quad", "Valley House Quad"};
 const char *GaddLiftStatus[GaddLiftCount] = {};
 
 // Castlerock Peak
 const int CastlerockTrailCount = 11;
-const char CastlerockTrailNames[CastlerockTrailCount] = {'Castlerock Run', 'Hi & Lo Road', 'Cotillion', 'Bailout', 'Lower Castlerock Run', 'Lower Lift Line', 'Runout', 'Troll Road', 'Rumble', 'Middle Earth', 'Lift Line'};
+const char* CastlerockTrailNames[CastlerockTrailCount] = {"Castlerock Run", "Hi & Lo Road", "Cotillion", "Bailout", "Lower Castlerock Run", "Lower Lift Line", "Runout", "Troll Road", "Rumble", "Middle Earth", "Lift Line"};
 const char *CastlerockTrailStatus[CastlerockTrailCount] = {};
 const char *CastlerockTrailRating[CastlerockTrailCount] = {};
 const int CastlerockLiftCount = 1;
-const char CastlerockLiftNames[CastlerockLiftCount] = {'Castlerock Double'};
+const char* CastlerockLiftNames[CastlerockLiftCount] = {"Castlerock Double"};
 const char *CastlerockLiftStatus[CastlerockLiftCount] = {};
 
 // North Lynx Peak
 const int NorthLynxTrailCount = 24;
-const char NorthLynxTrailNames[NorthLynxTrailCount] = {'Castlerock Connection', 'Birch Run', 'Morning Star', 'Sunrise', 'Hot Shot', 'Sleeper Chutes', 'Sleeper', 'Waterfall', 'Sleeper Road', 'Lower Hot Shot', 'First Time', 'Sugarbear Forest', 'Sugarbear Road', 'Easy Rider', 'Out Road', 'In Road', 'Out To Lunch', 'Lower Pushover', 'Slowpoke', 'Overshot', 'Pushover', 'Second Thoughts', 'Pushover Chute', 'Village Run'};
+const char* NorthLynxTrailNames[NorthLynxTrailCount] = {"Castlerock Connection", "Birch Run", "Morning Star", "Sunrise", "Hot Shot", "Sleeper Chutes", "Sleeper", "Waterfall", "Sleeper Road", "Lower Hot Shot", "First Time", "Sugarbear Forest", "Sugarbear Road", "Easy Rider", "Out Road", "In Road", "Out To Lunch", "Lower Pushover", "Slowpoke", "Overshot", "Pushover", "Second Thoughts", "Pushover Chute", "Village Run"};
 const char *NorthLynxTrailStatus[NorthLynxTrailCount] = {};
 const char *NorthLynxTrailRating[NorthLynxTrailCount] = {};
 const int NorthLynxLiftCount = 6;
-const char NorthLynxLiftNames[NorthLynxLiftCount] = {'North Lynx Triple', 'Gate House Express Quad', 'Schoolhouse Lift', 'Slide Brook Express Quad', 'Village Quad', 'Welcome Mat'};
+const char* NorthLynxLiftNames[NorthLynxLiftCount] = {"North Lynx Triple", "Gate House Express Quad", "Schoolhouse Lift", "Slide Brook Express Quad", "Village Quad", "Welcome Mat"};
 const char *NorthLynxLiftStatus[NorthLynxLiftCount] = {};
 
 // Mt Ellen
 const int EllenTrailCount = 36;
-const char EllenTrailNames[EllenTrailCount] = {'Black Diamond', 'Panorama', 'Rim Run', 'F.I.S.', 'Upper Lookin\' Good', 'Elbow', 'Lookin\' Good', 'Lower Rim Run', 'Spin Out', 'South Bound', 'Lower Elbow', 'Bravo', 'Exterminator', 'Lower Exterminator', 'Way Back', 'Lower F.I.S.', 'Snowflake', 'Graduation', 'Riemergasse', 'Sugar Run', 'Lower Crackerjack', 'Straight Shot', 'Crackerjack', 'Tumbler', 'The Cliffs', 'Hammerhead', 'Encore', 'Which Way', 'Cruiser', 'North Star', 'Northway', 'Northridge Expressway', 'Lower Northstar', 'Mainstream', 'Easy Does It', 'Rob Roy'};
+const char* EllenTrailNames[EllenTrailCount] = {"Black Diamond", "Panorama", "Rim Run", "F.I.S.", "Upper Lookin\' Good", "Elbow", "Lookin\' Good", "Lower Rim Run", "Spin Out", "South Bound", "Lower Elbow", "Bravo", "Exterminator", "Lower Exterminator", "Way Back", "Lower F.I.S.", "Snowflake", "Graduation", "Riemergasse", "Sugar Run", "Lower Crackerjack", "Straight Shot", "Crackerjack", "Tumbler", "The Cliffs", "Hammerhead", "Encore", "Which Way", "Cruiser", "North Star", "Northway", "Northridge Expressway", "Lower Northstar", "Mainstream", "Easy Does It", "Rob Roy"};
 const char *EllenTrailStatus[EllenTrailCount] = {};
 const char *EllenTrailRating[EllenTrailCount] = {};
 const int EllenLiftCount = 5;
-const char EllenLiftNames[EllenLiftCount] = {'Green Mountain Express Quad', 'North Ridge Express Quad', 'Summit Quad', 'Sunshine Quad', 'Tommy\'s Toy'};
+const char* EllenLiftNames[EllenLiftCount] = {"Green Mountain Express Quad", "North Ridge Express Quad", "Summit Quad", "Sunshine Quad", "Easy Up"};
 const char *EllenLiftStatus[EllenLiftCount] = {};
 
 // Inverness Peak
 const int InvernessTrailCount = 4;
-const char InvernessTrailNames[InvernessTrailCount] = {'Brambles', 'Inverness', 'Semi-Tough', 'Walt\'s Trail'};
+const char* InvernessTrailNames[InvernessTrailCount] = {"Brambles", "Inverness", "Semi-Tough", "Walt\'s Trail"};
 const char *InvernessTrailStatus[InvernessTrailCount] = {};
 const char *InvernessTrailRating[InvernessTrailCount] = {};
 const int InvernessLiftCount = 1;
-const char InvernessLiftNames[InvernessLiftCount] = {'Inverness Quad'};
+const char* InvernessLiftNames[InvernessLiftCount] = {"Inverness Quad"};
 const char *InvernessLiftStatus[InvernessLiftCount] = {};
 
 void setup() {
