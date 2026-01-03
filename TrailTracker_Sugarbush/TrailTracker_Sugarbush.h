debda2957a07404e7a47d5a4068fb2c5b062edd1
cpcurtin <71350501+cpcurtin@users.noreply.github.com>
2026-01-03 18:41:03 -0500

    Implemented struct for features. Fixed parse functions. Issue with difficulty parse.

--- TrailTracker_Sugarbush/TrailTracker_Sugarbush.h
+++ TrailTracker_Sugarbush/TrailTracker_Sugarbush.h
@@ -21,6 +21,16 @@
 // Define functions to toggle NeoPixels
 void offLED(Adafruit_NeoPixel, int);
 void onLED(Adafruit_NeoPixel, int, int, int, int);
+void parseTrailData(DynamicJsonDocument, struct Trail*, int, int, const char, const char*);
+void parseLiftData(DynamicJsonDocument, struct Trail*, int, int, const char, int);
+
+struct Trail{
+    int Status;
+    int Difficulty;
+    int Grooming;
+    int Snowmaking;
+};
+
 
 /*************************************************************************************************
 / This block outlines the trails on the Sugarbush property, broken out by peak. In their backend, they are split by peak, so this make it easier to parse the JSON. 
@@ -33,8 +43,6 @@ const char* PeakNameOrder[] = {"Lincoln Peak", "Gadd Peak", "Castlerock Peak", "
 // Lincoln Peak
 const int LincolnTrailCount = 9;
 const char* LincolnTrailNames[LincolnTrailCount] = {"Jester", "Allyn\'s Traverse", "Organgrinder", "Spillsville", "Sigi\'s Ripcord", "Paradise", "Lower Paradise", "Paradise Extension", "Lower Ripcord"};
-int LincolnTrailStatus[LincolnTrailCount];
-int LincolnTrailRating[LincolnTrailCount];
 const int LincolnLiftCount = 1;
 const char* LincolnLiftNames[LincolnLiftCount] = {"Heaven\'s Gate Quad"};
 int LincolnLiftStatus[LincolnLiftCount];
@@ -42,8 +50,6 @@ int LincolnLiftStatus[LincolnLiftCount];
 // Gadd Peak
 const int GaddTrailCount= 27;
 const char* GaddTrailNames[GaddTrailCount] = {"Domino Chute", "Downspout", "Domino", "Lower Downspout", "Lower Domino", "Heaven\'s Gate Traverse", "Lower Jester", "Lower Birdland", "Lower Organgrinder", "Murphy\'s Glades", "Birdland", "Valley House Traverse", "Stein\'s Run", "The Mall", "Snowball", "Lixi\'s Twist", "Moonshine", "Lower Twist", "Spring Fling", "Lower Snowball", "Spring Road", "Racer\'s Edge", "Cat\'s Meow", "Lower Moonshine", "Coffee Run", "Gondolier", "Header"};
-int GaddTrailStatus[GaddTrailCount];
-int GaddTrailRating[GaddTrailCount];
 const int GaddLiftCount = 2;
 const char* GaddLiftNames[GaddLiftCount] = {"Super Bravo Express Quad", "Valley House Quad"};
 int GaddLiftStatus[GaddLiftCount];
@@ -51,8 +57,6 @@ int GaddLiftStatus[GaddLiftCount];
 // Castlerock Peak
 const int CastlerockTrailCount = 11;
 const char* CastlerockTrailNames[CastlerockTrailCount] = {"Castlerock Run", "Hi & Lo Road", "Cotillion", "Bailout", "Lower Castlerock Run", "Lower Lift Line", "Runout", "Troll Road", "Rumble", "Middle Earth", "Lift Line"};
-int CastlerockTrailStatus[CastlerockTrailCount];
-int CastlerockTrailRating[CastlerockTrailCount];
 const int CastlerockLiftCount = 1;
 const char* CastlerockLiftNames[CastlerockLiftCount] = {"Castlerock Double"};
 int CastlerockLiftStatus[CastlerockLiftCount];
@@ -60,29 +64,20 @@ int CastlerockLiftStatus[CastlerockLiftCount];
 // North Lynx Peak
 const int NorthLynxTrailCount = 24;
 const char* NorthLynxTrailNames[NorthLynxTrailCount] = {"Castlerock Connection", "Birch Run", "Morning Star", "Sunrise", "Hot Shot", "Sleeper Chutes", "Sleeper", "Waterfall", "Sleeper Road", "Lower Hot Shot", "First Time", "Sugarbear Forest", "Sugarbear Road", "Easy Rider", "Out Road", "In Road", "Out To Lunch", "Lower Pushover", "Slowpoke", "Overshot", "Pushover", "Second Thoughts", "Pushover Chute", "Village Run"};
-int NorthLynxTrailStatus[NorthLynxTrailCount];
-int NorthLynxTrailRating[NorthLynxTrailCount];
 const int NorthLynxLiftCount = 6;
 const char* NorthLynxLiftNames[NorthLynxLiftCount] = {"North Lynx Triple", "Gate House Express Quad", "Schoolhouse Lift", "Slide Brook Express Quad", "Village Quad", "Welcome Mat"};
-int NorthLynxLiftStatus[NorthLynxLiftCount];
 
 // Mt Ellen
 const int EllenTrailCount = 36;
 const char* EllenTrailNames[EllenTrailCount] = {"Black Diamond", "Panorama", "Rim Run", "F.I.S.", "Upper Lookin\' Good", "Elbow", "Lookin\' Good", "Lower Rim Run", "Spin Out", "South Bound", "Lower Elbow", "Bravo", "Exterminator", "Lower Exterminator", "Way Back", "Lower F.I.S.", "Snowflake", "Graduation", "Riemergasse", "Sugar Run", "Lower Crackerjack", "Straight Shot", "Crackerjack", "Tumbler", "The Cliffs", "Hammerhead", "Encore", "Which Way", "Cruiser", "North Star", "Northway", "Northridge Expressway", "Lower Northstar", "Mainstream", "Easy Does It", "Rob Roy"};
-int EllenTrailStatus[EllenTrailCount];
-int EllenTrailRating[EllenTrailCount];
 const int EllenLiftCount = 5;
 const char* EllenLiftNames[EllenLiftCount] = {"Green Mountain Express Quad", "North Ridge Express Quad", "Summit Quad", "Sunshine Quad", "Easy Up"};
-int EllenLiftStatus[EllenLiftCount];
 
 // Inverness Peak
 const int InvernessTrailCount = 4;
 const char* InvernessTrailNames[InvernessTrailCount] = {"Brambles", "Inverness", "Semi-Tough", "Walt\'s Trail"};
-int InvernessTrailStatus[InvernessTrailCount];
-int InvernessTrailRating[InvernessTrailCount];
 const int InvernessLiftCount = 1;
 const char* InvernessLiftNames[InvernessLiftCount] = {"Inverness Quad"};
-int InvernessLiftStatus[InvernessLiftCount];
 
 /*Instantiation of LED "strips" on board. Each peak is defined as its own strip & data is parsed based on these strips & their 
  corresponding trails/lifts. */
--- TrailTracker_Sugarbush/TrailTracker_Sugarbush.ino
+++ TrailTracker_Sugarbush/TrailTracker_Sugarbush.ino
@@ -3,7 +3,8 @@
 
 void offLED(Adafruit_NeoPixel, int);
 void onLED(Adafruit_NeoPixel, int, int, int, int);
-void parseTrailData(DynamicJsonDocument, int*, int, int, const char, const char*, const char);
+void parseTrailData(DynamicJsonDocument, struct Trail*, int, int, const char, const char*);
+void parseLiftData(DynamicJsonDocument, struct Trail*, int, int, const char, int);
 
 void setup() {
   
@@ -32,9 +33,21 @@ void setup() {
   stripNorthLynx.begin();
   stripEllen.begin();
   stripInverness.begin();
+
 }
 
 void loop() {
+  struct Trail Lincoln[LincolnTrailCount + LincolnLiftCount]; 
+  struct Trail Gadd[GaddTrailCount + GaddLiftCount];
+  struct Trail Castlerock[CastlerockTrailCount + CastlerockLiftCount];
+  struct Trail NorthLynx[NorthLynxTrailCount + NorthLynxLiftCount];
+  struct Trail Ellen[EllenTrailCount + EllenTrailCount];
+  struct Trail Inverness[InvernessTrailCount + InvernessTrailCount];
+
+  /*for (int i = 0; i < LincolnTrailCount; i++){
+    const char* name = &&LincolnTrailNames[i];
+    strcpy(Lincoln[i].Name, name);
+  }*/
   //Handle HTTP requests & recording status
     HTTPClient https;
     Serial.print("[HTTPS] begin...\n");
@@ -51,7 +64,7 @@ void loop() {
           const String& payload = https.getString(); // Store the data returned by server
           //Serial.println(payload);
           // Convert data to something parseable
-          DynamicJsonDocument doc(100000);
+          DynamicJsonDocument doc(101000);
           DeserializationError error = deserializeJson(doc, payload);
           if (error){ // Check for error in deserialization
             Serial.print("Error! Deserialize failed!\n");
@@ -60,165 +73,143 @@ void loop() {
           else{
             Serial.print("No Error!\n");
             Serial.print("Lincoln Peak\n");
-            /*for (int i = 0; i < LincolnTrailCount; i++){ // Lincoln Peak
-              Serial.println(doc["Resorts"][0]["MountainAreas"][2]["Trails"][i]["Name"].as<String>());
-              LincolnTrailStatus[i] = doc["Resorts"][0]["MountainAreas"][2]["Trails"][i]["Status"].as<const char*>();
-              Serial.println(LincolnTrailStatus[i]);
-              LincolnTrailRating[i] = doc["Resorts"][0]["MountainAreas"][2]["Trails"][i]["Difficulty"].as<const char*>();
-              Serial.println(LincolnTrailRating[i]);
-              if ((doc["Resorts"][0]["MountainAreas"][2]["Trails"][i]["Status"].as<String>() == "Open") && i < LED_Count){
+
+            parseTrailData(doc, Lincoln, LincolnTrailCount, 2, "Trails", LincolnTrailNames);
+            parseLiftData(doc, Lincoln, LincolnTrailCount, 2, "Lifts", LincolnLiftCount);
+            for (int i = 0; i < (LincolnTrailCount + LincolnLiftCount); i++){
+              if (Lincoln[i].Status == 0){ // If Open
                 stripLincoln.setPixelColor(i, 0, Brightness, 0);
               }
-              else if ((doc["Resorts"][0]["MountainAreas"][2]["Trails"][i]["Status"].as<String>() == "Closed") && i < LED_Count){
+              else if (Lincoln[i].Status == 1){ // If Not Open
                 stripLincoln.setPixelColor(i, Brightness, 0, 0);
               }
-            }*/
-            //int LincolnTrailStatus[LincolnTrailCount];
-            parseTrailData(doc, LincolnTrailStatus, LincolnTrailCount, 2, "Trails", LincolnTrailNames, "Status");
-            for (int i = 0; i < LincolnTrailCount; i++){
-              if (LincolnTrailStatus[i] == 0){ // If Open
-                stripLincoln.setPixelColor(i, 0, Brightness, 0);
-              }
-              else if (LincolnTrailStatus[i] == 1){ // If Not Open
-                stripLincoln.setPixelColor(i, Brightness, 0, 0);
+              else if (Lincoln[i].Status == 2){ // If Open for Hiking
+                stripLincoln.setPixelColor(i, Brightness, 2, 0);
               }
             }
             stripLincoln.show();
 
-            Serial.print("Lincoln Lifts\n");
+            /*Serial.print("Lincoln Lifts\n");
             for (int i = 0; i < LincolnLiftCount; i++){
               Serial.println(doc["Resorts"][0]["MountainAreas"][2]["Lifts"][i]["Name"].as<String>());
               //LincolnLiftStatus[i] = doc["Resorts"][0]["MountainAreas"][2]["Lifts"][i]["Status"].as<const char*>();
               Serial.println(LincolnLiftStatus[i]);
-            }
+            }*/
 
             Serial.print("Gadd Peak\n");
-            parseTrailData(doc, GaddTrailStatus, GaddTrailCount, 3, "Trails", GaddTrailNames, "Status");
-            for (int i = 0; i < GaddTrailCount; i++){
-              if (GaddTrailStatus[i] == 0){ // If Open
+            parseTrailData(doc, Gadd, GaddTrailCount, 3, "Trails", GaddTrailNames);
+            parseLiftData(doc, Gadd, GaddTrailCount, 3, "Lifts", GaddLiftCount);
+            for (int i = 0; i < (GaddTrailCount + GaddLiftCount); i++){
+              if (Gadd[i].Status == 0){ // If Open
                 stripGadd.setPixelColor(i, 0, Brightness, 0);
               }
-              else if (GaddTrailStatus[i] == 1){ // If Not Open
+              else if (Gadd[i].Status == 1){ // If Not Open
                 stripGadd.setPixelColor(i, Brightness, 0, 0);
+              }
+              else if (Gadd[i].Status == 2){ // If Open for Hiking
+                stripGadd.setPixelColor(i, Brightness, 2, 0);
               }
             }
             stripGadd.show();
-            /*for (int i = 0; i < GaddTrailCount; i++){
-              Serial.println(doc["Resorts"][0]["MountainAreas"][3]["Trails"][i]["Name"].as<String>());
-              Serial.println(doc["Resorts"][0]["MountainAreas"][3]["Trails"][i]["Status"].as<String>());
-              GaddTrailStatus[i] = doc["Resorts"][0]["MountainAreas"][3]["Trails"][i]["Status"].as<const char*>();
-              Serial.println(GaddTrailStatus[i]);
-              GaddTrailRating[i] = doc["Resorts"][0]["MountainAreas"][3]["Trails"][i]["Difficulty"].as<const char*>();
-              Serial.println(GaddTrailRating[i]);
-            }*/
-            Serial.print("Gadd Lifts\n");
+
+            /*Serial.print("Gadd Lifts\n");
             for (int i = 0; i < GaddLiftCount; i++){
               Serial.println(doc["Resorts"][0]["MountainAreas"][3]["Lifts"][i]["Name"].as<String>());
               //GaddLiftStatus[i] = doc["Resorts"][0]["MountainAreas"][3]["Lifts"][i]["Status"].as<const char*>();
               Serial.println(GaddLiftStatus[i]);
-            }
+            }*/
 
             Serial.print("Castlerock Peak\n");
-            parseTrailData(doc, CastlerockTrailStatus, CastlerockTrailCount, 4, "Trails", CastlerockTrailNames, "Status");
-            for (int i = 0; i < CastlerockTrailCount; i++){
-              if (CastlerockTrailStatus[i] == 0){ // If Open
+            parseTrailData(doc, Castlerock, CastlerockTrailCount, 4, "Trails", CastlerockTrailNames);
+            parseLiftData(doc, Castlerock, CastlerockTrailCount, 4, "Trails", CastlerockLiftCount);
+            for (int i = 0; i < (CastlerockTrailCount + CastlerockLiftCount); i++){
+              if (Castlerock[i].Status == 0){ // If Open
                 stripCastlerock.setPixelColor(i, 0, Brightness, 0);
               }
-              else if (CastlerockTrailStatus[i] == 1){ // If Not Open
+              else if (Castlerock[i].Status == 1){ // If Not Open
                 stripCastlerock.setPixelColor(i, Brightness, 0, 0);
+              }
+              else if (Castlerock[i].Status == 2){ // If Open for Hiking
+                stripCastlerock.setPixelColor(i, Brightness, 2, 0);
               }
             }
             stripCastlerock.show();
-            /*for (int i = 0; i < CastlerockTrailCount; i++){
-              Serial.println(doc["Resorts"][0]["MountainAreas"][4]["Trails"][i]["Name"].as<String>());
-              Serial.println(doc["Resorts"][0]["MountainAreas"][4]["Trails"][i]["Status"].as<String>());
-              CastlerockTrailStatus[i] = doc["Resorts"][0]["MountainAreas"][4]["Trails"][i]["Status"].as<const char*>();
-              Serial.println(CastlerockTrailStatus[i]);
-              CastlerockTrailRating[i] = doc["Resorts"][0]["MountainAreas"][4]["Trails"][i]["Difficulty"].as<const char*>();
-              Serial.println(CastlerockTrailRating[i]);
-            }*/
-            Serial.print("Castlerock Lifts\n");
+
+            /*Serial.print("Castlerock Lifts\n");
             for (int i = 0; i < CastlerockLiftCount; i++){
               Serial.println(doc["Resorts"][0]["MountainAreas"][4]["Lifts"][i]["Name"].as<String>());
               //CastlerockLiftStatus[i] = doc["Resorts"][0]["MountainAreas"][4]["Lifts"][i]["Status"].as<const char*>();
               Serial.println(CastlerockLiftStatus[i]);
-            }
+            }*/
+
             Serial.print("North Lynx Peak\n");
-            parseTrailData(doc, NorthLynxTrailStatus, NorthLynxTrailCount, 5, "Trails", NorthLynxTrailNames, "Status");
-            for (int i = 0; i < NorthLynxTrailCount; i++){
-              if (NorthLynxTrailStatus[i] == 0){ // If Open
+            parseTrailData(doc, NorthLynx, NorthLynxTrailCount, 5, "Trails", NorthLynxTrailNames);
+            parseLiftData(doc, NorthLynx, NorthLynxTrailCount, 5, "Lifts", NorthLynxLiftCount);
+            for (int i = 0; i < (NorthLynxTrailCount + NorthLynxLiftCount); i++){
+              if (NorthLynx[i].Status == 0){ // If Open
                 stripNorthLynx.setPixelColor(i, 0, Brightness, 0);
               }
-              else if (NorthLynxTrailStatus[i] == 1){ // If Not Open
+              else if (NorthLynx[i].Status == 1){ // If Not Open
                 stripNorthLynx.setPixelColor(i, Brightness, 0, 0);
+              }
+              else if (NorthLynx[i].Status == 2){ // If Open for Hiking
+                stripNorthLynx.setPixelColor(i, Brightness, 2, 0);
               }
             }
             stripNorthLynx.show();
-            /*for (int i = 0; i < NorthLynxTrailCount; i++){
-              Serial.println(doc["Resorts"][0]["MountainAreas"][5]["Trails"][i]["Name"].as<String>());
-              Serial.println(doc["Resorts"][0]["MountainAreas"][5]["Trails"][i]["Status"].as<String>());
-              NorthLynxTrailStatus[i] = doc["Resorts"][0]["MountainAreas"][5]["Trails"][i]["Status"].as<const char*>();
-              Serial.println(NorthLynxTrailStatus[i]);
-              NorthLynxTrailRating[i] = doc["Resorts"][0]["MountainAreas"][5]["Trails"][i]["Difficulty"].as<const char*>();
-              Serial.println(NorthLynxTrailRating[i]);
-            }*/
-            Serial.print("North Lynx Lifts\n");
+
+            /*Serial.print("North Lynx Lifts\n");
             for (int i = 0; i < NorthLynxLiftCount; i++){
               Serial.println(doc["Resorts"][0]["MountainAreas"][5]["Lifts"][i]["Name"].as<String>());
               //NorthLynxLiftStatus[i] = doc["Resorts"][0]["MountainAreas"][5]["Lifts"][i]["Status"].as<const char*>();
               Serial.println(NorthLynxLiftStatus[i]);
-            }
+            }*/
 
             Serial.print("Mt. Ellen\n");
-            parseTrailData(doc, EllenTrailStatus, EllenTrailCount, 6, "Trails", EllenTrailNames, "Status");
-            for (int i = 0; i < EllenTrailCount; i++){
-              if (EllenTrailStatus[i] == 0){ // If Open
+            parseTrailData(doc, Ellen, EllenTrailCount, 6, "Trails", EllenTrailNames);
+            parseLiftData(doc, Ellen, EllenTrailCount, 6, "Lifts", EllenLiftCount);
+            for (int i = 0; i < (EllenTrailCount + EllenLiftCount); i++){
+              if (Ellen[i].Status == 0){ // If Open
                 stripEllen.setPixelColor(i, 0, Brightness, 0);
               }
-              else if (EllenTrailStatus[i] == 1){ // If Not Open
+              else if (Ellen[i].Status == 1){ // If Not Open
                 stripEllen.setPixelColor(i, Brightness, 0, 0);
+              }
+              else if (Ellen[i].Status == 2){ // If Open for Hiking
+                stripEllen.setPixelColor(i, Brightness, 2, 0);
               }
             }
             stripEllen.show();
-            /*for(int i = 0; i < EllenTrailCount; i++){
-              Serial.println(doc["Resorts"][0]["MountainAreas"][6]["Trails"][i]["Name"].as<String>());
-              Serial.println(doc["Resorts"][0]["MountainAreas"][6]["Trails"][i]["Status"].as<String>());
-              EllenTrailStatus[i] = doc["Resorts"][0]["MountainAreas"][6]["Trails"][i]["Status"].as<const char*>();
-              Serial.println(EllenTrailStatus[i]);
-              EllenTrailRating[i] = doc["Resorts"][0]["MountainAreas"][6]["Trails"][i]["Difficulty"].as<const char*>();
-              Serial.println(EllenTrailRating[i]);
-            }*/
-            Serial.print("Ellen Lifts\n");
+
+            /*Serial.print("Ellen Lifts\n");
             for (int i = 0; i < EllenLiftCount; i++){
               Serial.println(doc["Resorts"][0]["MountainAreas"][6]["Lifts"][i]["Name"].as<String>());
               //EllenLiftStatus[i] = doc["Resorts"][0]["MountainAreas"][6]["Lifts"][i]["Status"].as<const char*>();
               Serial.println(EllenLiftStatus[i]);
-            }
+            }*/
+
             Serial.print("Inverness Peak\n");
-            parseTrailData(doc,InvernessTrailStatus, InvernessTrailCount, 7, "Trails", InvernessTrailNames, "Status");
-            for (int i = 0; i < InvernessTrailCount; i++){
-              if (InvernessTrailStatus[i] == 0){ // If Open
+            parseTrailData(doc, Inverness, InvernessTrailCount, 7, "Trails", InvernessTrailNames);
+            parseLiftData(doc, Inverness, InvernessTrailCount, 7, "Lifts", InvernessLiftCount);
+            for (int i = 0; i < (InvernessTrailCount + InvernessLiftCount); i++){
+              if (Inverness[i].Status == 0){ // If Open
                 stripInverness.setPixelColor(i, 0, Brightness, 0);
               }
-              else if (InvernessTrailStatus[i] == 1){ // If Not Open
+              else if (Inverness[i].Status == 1){ // If Not Open
                 stripInverness.setPixelColor(i, Brightness, 0, 0);
+              }
+              else if (Inverness[i].Status == 2){ // If Open for Hiking
+                stripInverness.setPixelColor(i, Brightness, 2, 0);
               }
             }
             stripInverness.show();
-            /*for (int i = 0; i < InvernessTrailCount; i++){
-              Serial.println(doc["Resorts"][0]["MountainAreas"][7]["Trails"][i]["Name"].as<String>());
-              Serial.println(doc["Resorts"][0]["MountainAreas"][7]["Trails"][i]["Status"].as<String>());
-              InvernessTrailStatus[i] = doc["Resorts"][0]["MountainAreas"][7]["Trails"][i]["Status"].as<const char*>();
-              Serial.println(InvernessTrailStatus[i]);
-              InvernessTrailRating[i] = doc["Resorts"][0]["MountainAreas"][7]["Trails"][i]["Difficulty"].as<const char*>();
-              Serial.println(InvernessTrailRating[i]);
-            }*/
-            Serial.print("Inverness Lifts\n");
+
+            /*Serial.print("Inverness Lifts\n");
             for (int i = 0; i < InvernessLiftCount; i++){
               Serial.println(doc["Resorts"][0]["MountainAreas"][7]["Lifts"][i]["Name"].as<String>());
               //InvernessLiftStatus[i] = doc["Resorts"][0]["MountainAreas"][7]["Lifts"][i]["Status"].as<const char*>();
               Serial.println(InvernessLiftStatus[i]);
-            }
+            }*/
           }         
         }
       } 
@@ -251,19 +242,70 @@ void onLED(Adafruit_NeoPixel &strip, int LEDposition, int red, int green, int bl
   strip.show();
 }
 
-void parseTrailData(DynamicJsonDocument doc, int *trailStatus, int peakCount, int peakNumber, const char* featureType, const char** featureNames, const char* desiredData){
-  for (int i = 0; i < peakCount; i++){ // Lincoln Peak
-    Serial.println(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i][desiredData].as<String>());
-    if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i][desiredData].as<const char*>(), "Open") == 0){
-      trailStatus[i] = 0; // If trail is open, put 0
+void parseTrailData(DynamicJsonDocument doc, struct Trail* Peak, int peakCount, int peakNumber, const char* featureType, const char** featureNames){
+  for (int i = 0; i < peakCount; i++){ 
+    Serial.println(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Status"].as<String>());
+    // Status
+    if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Status"].as<const char*>(), "Open") == 0){
+      Peak[i].Status = 0; // If trail is open, put 0
     }
-    else if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i][desiredData].as<const char*>(), "Closed") == 0){
-      trailStatus[i] = 1; // If trail is closed, put 1
+    else if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Status"].as<const char*>(), "Closed") == 0){
+      Peak[i].Status = 1; // If trail is closed, put 1
     }
-    else if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i][desiredData].as<const char*>(), "Open for Hiking") == 0){
-      trailStatus[i] = 2; // If trail is open for hiking, put 2
+    else if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Status"].as<const char*>(), "Open for Hiking") == 0){
+      Peak[i].Status = 2; // If trail is open for hiking, put 2
     }
     //Serial.println(LincolnTrailNames[i]);
-    Serial.println(trailStatus[i]);
+    Serial.println(Peak[i].Status);
+
+    // Difficulty
+    Serial.println(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Difficulty"].as<String>());
+    if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Difficulty"].as<const char*>(), "Beginner")){
+      Peak[i].Difficulty = 0; // Beginner
+    }
+    else if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Difficulty"].as<const char*>(), "Intermediate")){
+      Peak[i].Difficulty = 1; // Intermediate
+    }
+    else if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Difficulty"].as<const char*>(), "Difficult")){
+      Peak[i].Difficulty = 2; // Advanced
+    }
+    else if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Difficulty"].as<const char*>(), "Expert")){
+      Peak[i].Difficulty = 3; // Expert
+    }
+    Serial.println(Peak[i].Difficulty);
+
+    // Grooming
+    if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Grooming"].as<const char*>(), "No")){
+      Peak[i].Grooming = 0; // If no grooming
+    }
+    else if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Grooming"].as<const char*>(), "Yes")){
+      Peak[i].Grooming = 1; // If grooming
+    }
+    Serial.println(Peak[i].Grooming);
+
+    // Snowmaking
+    if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["SnowMaking"].as<const char*>(), "No")){
+      Peak[i].Snowmaking = 0; // If no snowmaking
+    }
+    else if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["SnowMaking"].as<const char*>(), "Yes")){
+      Peak[i].Snowmaking = 1; // If snowmaking
+    }
+    Serial.println(Peak[i].Snowmaking);
+  }
+}
+
+void parseLiftData(DynamicJsonDocument doc, struct Trail* Lift, int peakCount, int peakNumber, const char*featureType, int liftCount){
+  for (int i = 0; i < liftCount; i++){
+    Serial.println(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Status"].as<String>());
+    if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Status"].as<const char*>(), "Open") == 0){
+      Lift[peakCount + i].Status = 0; // If lift open
+    }
+    else if (strcmp(doc["Resorts"][0]["MountainAreas"][peakNumber][featureType][i]["Status"].as<const char*>(), "Closed") == 0){
+      Lift[peakCount + i].Status = 1; // If lift closed
+    }
+    else {
+      Lift[peakCount + i].Status = 1;
+    }
+    Serial.println(Lift[peakCount + i].Status);
   }
 }
